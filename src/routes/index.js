ee32920e8a025e43afcebeeab72dba2c01681873
Nguyên <nguyentrandn33@gmail.com>
2022-07-25 21:44:42 +0700

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ import Home from "../pages/Home";
 import DetailFilm from "../pages/DetailFilm";
 
 const publicRoutes = [
-    {path: "/home", component: Home},
-    {path: `/filmDetail/`, component: DetailFilm}
+    {path: "/film/home", component: Home},
+    {path: `/film/filmDetail/`, component: DetailFilm}
 ]
 
 export {publicRoutes}
